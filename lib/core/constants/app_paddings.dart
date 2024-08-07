15196129e67b976971de6a3cf328c038608a2d6a
memrec45 <65484287+memrec45@users.noreply.github.com>
2024-08-07 12:12:09 +0300

    AppPaddings class'ı eklendi

---
+++ lib/core/constants/app_paddings.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppPaddings {
+  /// Sign In , Sign Up ve Welcome sayfalarında kullanılan horizontal padding.
+  /// Padding : 20 px
+  static const EdgeInsets authScreenHorizontalPadding =
+      EdgeInsets.symmetric(horizontal: 20);
+
+  /// Onboarding sayfalarında kullanılan horizontal padding.
+  /// Padding : 28 px
+  static const EdgeInsets onboardingScreenHorizontalPadding =
+      EdgeInsets.symmetric(horizontal: 28);
+
+  /// Home sayfasında kullanılan horizontal padding.
+  /// Padding : 8 px
+  ///! NOT : Bu ileride değiştirilebilir.
+  static const EdgeInsets homeScreenHorizontalPadding =
+      EdgeInsets.symmetric(horizontal: 8);
+
+  /// 20 px.
+  static const double smallPaddingValue = 20.0;
+
+  /// 30 px.
+  static const double mediumPaddingValue = 30.0;
+
+  /// 40 px.
+  static const double largePaddingValue = 40.0;
+}
