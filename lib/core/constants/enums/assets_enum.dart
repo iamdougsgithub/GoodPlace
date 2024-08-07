0f0358832c4842dc7cd1f5e427e39d8eccafb9d2
memrec45 <65484287+memrec45@users.noreply.github.com>
2024-08-07 12:27:06 +0300

    AssetsEnum eklendi.

---
+++ lib/core/constants/enums/assets_enum.dart
@@ -0,0 +1,24 @@
+enum AssetsEnum {
+  googleIcon("${_rootIconPath}ic_google.png"),
+
+  /// Average per daily card'ında kullanılan icon.
+  averagePerDailyCardIcon("${_rootIconPath}ic_avarage_per_daily.png"),
+
+  /// Sign In ve Sign Up sayfalarında kullanılan image
+  authTopBackgroundImage("${_rootImagePath}auth_screens_image.png"),
+  welcomePageImage("${_rootImagePath}img_welcome.png"),
+
+  onboardingFirstPageImage("${_rootImagePath}img_Onbarding_1.png"),
+  onboardingSecondPageImage("${_rootImagePath}img_Onbarding_2.png"),
+  onboardingThirdPageImage("${_rootImagePath}img_Onbarding_3.png"),
+  onboardingFourthPageImage("${_rootImagePath}img_Onbarding_4.png"),
+  ;
+
+  static const String _rootIconPath = "lib/core/assets/icons/";
+  static const String _rootImagePath = "lib/core/assets/images/";
+
+  final String path;
+
+  /// This enum contains Asset paths.
+  const AssetsEnum(this.path);
+}
