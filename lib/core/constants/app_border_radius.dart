87cd91e874fbc01151190c7e2174c9c56026bda7
memrec45 <65484287+memrec45@users.noreply.github.com>
2024-08-07 12:19:41 +0300

    AppBorderRadius class eklendi

---
+++ lib/core/constants/app_border_radius.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppBorderRadius {
+  /// TextField'lar için kullanılan border radius
+  /// BorderRadius value : 15
+  static BorderRadius textFieldBorderRadius =
+      BorderRadius.circular(textFieldBorderRadiusValue);
+
+  /// xSmall circular border radius
+  /// value : 8px
+  static BorderRadius xsmallBorderRadius =
+      BorderRadius.circular(xsmallBorderRadiusValue);
+
+  /// Small circular border radius
+  /// value : 10px
+  static BorderRadius smallBorderRadius =
+      BorderRadius.circular(smallBorderRadiusValue);
+
+  /// Medium circular border radius
+  /// value : 22px
+  static BorderRadius mediumBorderRadius =
+      BorderRadius.circular(mediumBorderRadiusValue);
+
+  /// Large circular border radius
+  /// value : 30px
+  static BorderRadius largeBorderRadius =
+      BorderRadius.circular(largeBorderRadiusValue);
+
+  static const double textFieldBorderRadiusValue = 15.0;
+  static const double xsmallBorderRadiusValue = 8.0;
+  static const double smallBorderRadiusValue = 10.0;
+  static const double mediumBorderRadiusValue = 22.0;
+  static const double largeBorderRadiusValue = 30.0;
+}
