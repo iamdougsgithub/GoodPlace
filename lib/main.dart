5f7f1d360012e6c95410c685b0d1feff647de546
Havva Nur Ezginci <havvanurezginci.repository@gmail.com>
2024-08-09 19:31:36 +0300

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:good_place/config/theme.dart';
 import 'config/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
