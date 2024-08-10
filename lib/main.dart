a7bf445fd06679da5b41cfb71c35c14a0cb26a38
memrec45 <65484287+memrec45@users.noreply.github.com>
2024-08-10 22:08:25 +0300

    DeviceOrientation.portraitUp eklendi .

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:good_place/config/theme.dart';
 import 'config/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
