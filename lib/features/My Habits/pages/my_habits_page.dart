9f53a569b3780695f24b1377acdb7b6e0e597b0b
memrec45 <65484287+memrec45@users.noreply.github.com>
2024-08-14 17:01:41 +0300

    Refactor: HabitTile moved to habit_tile.dart file

--- lib/features/My Habits/pages/my_habits_page.dart
+++ lib/features/My Habits/pages/my_habits_page.dart
@@ -11,6 +11,7 @@ import '../../../core/utils/widgets/card_background_cover.dart';
 import '../../home/widgets/i_drawer.dart';
 
 import '../../../config/theme.dart';
+import '../widgets/habit_tile.dart';
 
 class MyHabitsPage extends StatefulWidget {
   static const String routeName = "my habits";
@@ -111,118 +112,6 @@ class _MyHabitsPageState extends State<MyHabitsPage> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class HabitTile extends StatelessWidget {
-  const HabitTile({super.key});
-  final String streakCount = "5";
-  final String habitName = "Drink Water";
-  final String habitPurpose = "Lorem ipsum dolor sit amet consectetur.";
-  final String habitCreatedDate = "14.08.24";
-
-  final String imgUrl =
-      "https://plus.unsplash.com/premium_photo-1665990294432-c4baf9088c50?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: AppBorderRadius.smallBorderRadius,
-      child: Slidable(
-        startActionPane: checkButton(),
-        endActionPane: deleteButton(),
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: NetworkImage(imgUrl),
-            ),
-          ),
-          child: CardBackgroundImageFilter(
-            child: ListTile(
-              dense: true,
-              contentPadding: EdgeInsets.symmetric(
-                vertical: AppPaddings.mediumPaddingValue,
-                horizontal: AppPaddings.smallPaddingValue,
-              ),
-              title: FittedBox(
-                fit: BoxFit.fitHeight,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    contentColumn(context),
-                    Gap(AppPaddings.largePaddingValue),
-                    streakColumn(context),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  ActionPane deleteButton() {
-    return ActionPane(
-      motion: Container(
-        color: AppColors.errDark,
-        child: IconButton(
-          onPressed: () {},
-          icon: AppAssets.removeIcon,
-        ),
-      ),
-      extentRatio: 0.3,
-      children: [],
-    );
-  }
-
-  ActionPane checkButton() {
-    return ActionPane(
-      motion: Container(
-        color: AppColors.succDark,
-        child: IconButton(
-          onPressed: () {},
-          icon: AppAssets.checkIcon,
-        ),
-      ),
-      extentRatio: 0.3,
-      children: [],
-    );
-  }
-
-  Column streakColumn(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          streakCount,
-          style: context.textTheme.headlineLarge,
-        ),
-        Text(
-          "Streak",
-          style: context.textTheme.titleSmall,
-        ),
-      ],
-    );
-  }
-
-  Column contentColumn(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          habitName,
-          style: context.textTheme.titleMedium,
-        ),
-        Text(
-          habitPurpose,
-          style: context.textTheme.labelMedium,
-        ),
-        Text(
-          habitCreatedDate,
-          style: context.textTheme.labelSmall,
-        ),
-      ],
     );
   }
 }
