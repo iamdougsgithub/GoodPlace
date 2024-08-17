33c8b72d42fe686fd441d1a994cf2832fe11b493
Havva Nur Ezginci <havvanurezginci.repository@gmail.com>
2024-08-17 14:17:35 +0300

    add: habitProvider

---
+++ lib/features/user_data/habit_provider.dart
@@ -0,0 +1,101 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:good_place/core/utils/models/habit_model.dart';
+import 'package:good_place/features/user_data/user_service.dart';
+
+class HabitProvider with ChangeNotifier {
+  List<HabitModel> _habits = [];
+
+  List<HabitModel> get habits => _habits;
+
+  final UserDatabaseService _userService = UserDatabaseService();
+
+  Timer? _midnightTimer;
+
+  Future<void> getHabits() async {
+    _habits = await _userService.getUserHabits();
+    if (_midnightTimer == null) {
+      _startMidnightTimer();
+    }
+    notifyListeners();
+  }
+
+  Future<void> addHabit(HabitModel habit) async {
+    DateTime now = DateTime.now(); // Mevcut tarihi al
+
+    habit.id = await _userService.addHabit(HabitModel(
+        title: "Günde 1 saat yürüyüş",
+        createDate: now,
+        streakCount: 2,
+        imageUrl:
+            "https://www.multibem.com.tr/wp-content/uploads/2022/11/unnamed-1024x619.jpg",
+        completionDates: [])); // bu HabitModel örnek amaçlı koyuldu kaldırılacak.
+
+    if (habit.id!.isNotEmpty) {
+      _habits.add(habit);
+      notifyListeners();
+    } else {
+      print("Ekelem yapılamadı bir hata oluştu.");
+      print("Buraya bir uyarı-hata mesajı eklenebilir");
+    }
+  }
+
+//update: streakCount ve completionDates e bugünün tarihi eklenecek
+  Future<void> updateHabit(
+      String habitId, Map<String, dynamic> updatedFields) async {
+    String newTitle = 'Updated Habit Title';
+
+    int index = _habits.indexWhere((h) => h.id == habitId);
+
+    DateTime now = DateTime.now();
+
+    Map<String, dynamic> updatedFields = {
+      'completionDates': FieldValue.arrayUnion([now]),
+      'title': newTitle,
+    };
+
+    _userService.updateHabitFields("wP331iUnModp4nqz9v7x",
+        updatedFields); //habitId dinamik hale getir deneme amaçlı
+
+    _habits[index].completionDates.add(now);
+    _habits[index].title = updatedFields["title"];
+
+    notifyListeners();
+  }
+
+  // Habit sil
+  Future<void> deleteHabit(String habitId) async {
+    await _userService.deleteHabit(habitId);
+
+    _habits.removeWhere((habit) => habit.id == habitId);
+    notifyListeners();
+  }
+
+  // Gece yarısında tüm alışkanlıkların done durumunu sıfırlayan timer
+  void _startMidnightTimer() {
+    DateTime now = DateTime.now();
+    DateTime midnight =
+        DateTime(now.year, now.month, now.day + 1); // Ertesi gece yarısı
+    Duration timeUntilMidnight = midnight.difference(now);
+
+    print("timeUntilMidnight:$timeUntilMidnight  midnight:$midnight now:$now ");
+
+    _midnightTimer = Timer(timeUntilMidnight, _resetHabitsAtMidnight);
+  }
+
+  void _resetHabitsAtMidnight() {
+    _habits.forEach((habit) {
+      habit.done = false;
+    });
+    notifyListeners();
+    _startMidnightTimer(); // Timer'ı ertesi gece için yeniden başlat
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _midnightTimer?.cancel();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,26 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:good_place/config/theme.dart';
+import 'package:good_place/features/user_data/habit_provider.dart';
+import 'package:good_place/firebase_options.dart';
+import 'package:provider/provider.dart';
 import 'package:toastification/toastification.dart';
 import 'config/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => HabitProvider(),
+      child: const MyApp(),
+    ),
+  );
   runApp(const MyApp());
 }
 
