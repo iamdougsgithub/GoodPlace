f076947dc5c64623d1900843f7f5c4539074e1ab
memrec45 <65484287+memrec45@users.noreply.github.com>
2024-08-19 13:37:28 +0300

    Add: Navigation to HabitDetail added to HabitCard

--- lib/features/home/widgets/my_habits_section.dart
+++ lib/features/home/widgets/my_habits_section.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:good_place/core/constants/app_assets.dart';
 import 'package:good_place/core/utils/models/habit_model.dart';
+import 'package:good_place/features/habit%20detail/pages/habit_detail.dart';
 import 'package:good_place/features/home/widgets/habit_list_builder.dart';
 import 'package:good_place/features/user_data/habit_provider.dart';
+import 'package:good_place/logger.dart';
 import 'package:provider/provider.dart';
 import '../../../core/utils/widgets/card_background_cover.dart';
 import '../../../core/constants/app_border_radius.dart';
@@ -111,50 +113,56 @@ class _HabitCardState extends State<_HabitCard> {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 16 / 10,
-      child: Container(
-        clipBehavior: Clip.antiAlias,
-        margin: const EdgeInsets.symmetric(
-          horizontal: AppPaddings.xxsmallPaddingValue,
-        ),
-        decoration: _boxDecoration(),
-        child: CardBackgroundImageFilter(
-          child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: AppPaddings.xxsmallPaddingValue,
-              ),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: CheckboxListTile(
-                  value: _isDone,
-                  contentPadding: EdgeInsets.zero,
-
-                  /// Title
-                  title: Text(
-                    _title,
-                    style: context.textTheme.titleMedium?.copyWith(
-                      color: Colors.white,
-                    ),
-                  ),
-
-                  onChanged: (_) {
-                    setState(() {
-                      _isDone = !_isDone;
-                    });
-
-                    /// TODO: Burayı sonra yap.
-                    // HabitProvider.instance.updateHabit(
-                    //   widget.habitModel.id ?? "",
-                    //   {
-                    //     "completionDates": FieldValue.arrayUnion([
-                    //       DateTime.now(),
-                    //     ]),
-                    //   },
-                    // );
-                  },
+    return GestureDetector(
+      onTap: () => context.navigator.pushNamed(
+        HabitDetail.routeName,
+        arguments: widget.habitModel,
+      ),
+      child: AspectRatio(
+        aspectRatio: 16 / 10,
+        child: Container(
+          clipBehavior: Clip.antiAlias,
+          margin: const EdgeInsets.symmetric(
+            horizontal: AppPaddings.xxsmallPaddingValue,
+          ),
+          decoration: _boxDecoration(),
+          child: CardBackgroundImageFilter(
+            child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: AppPaddings.xxsmallPaddingValue,
                 ),
-              )),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: CheckboxListTile(
+                    value: _isDone,
+                    contentPadding: EdgeInsets.zero,
+
+                    /// Title
+                    title: Text(
+                      _title,
+                      style: context.textTheme.titleMedium?.copyWith(
+                        color: Colors.white,
+                      ),
+                    ),
+
+                    onChanged: (_) {
+                      setState(() {
+                        _isDone = !_isDone;
+                      });
+
+                      /// TODO: Burayı sonra yap.
+                      // HabitProvider.instance.updateHabit(
+                      //   widget.habitModel.id ?? "",
+                      //   {
+                      //     "completionDates": FieldValue.arrayUnion([
+                      //       DateTime.now(),
+                      //     ]),
+                      //   },
+                      // );
+                    },
+                  ),
+                )),
+          ),
         ),
       ),
     );
