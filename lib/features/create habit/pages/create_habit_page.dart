3f75765a7a4d5690a6d234c50a99cedd4988e8cd
memrec45 <65484287+memrec45@users.noreply.github.com>
2024-08-28 21:52:09 +0300

    Fix:  CreateHabitPage doesn't give any error to user problem fixed

--- lib/features/create habit/pages/create_habit_page.dart
+++ lib/features/create habit/pages/create_habit_page.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:good_place/features/chatgpt/SystemContentTexts.dart';
-import 'package:lottie/lottie.dart';
+import 'package:good_place/features/create%20habit/widgets/create_habit_form.dart';
 
 import 'package:provider/provider.dart';
 
 import '../../../config/theme.dart';
-import '../../../core/constants/app_assets.dart';
 import '../../../core/constants/app_paddings.dart';
 import '../../../core/extensions/context_extension.dart';
 import '../../../core/utils/models/habit_model.dart';
-import '../../../core/utils/widgets/custom_text_form_field.dart';
-import '../../chatgpt/ChatGptService.dart';
 import '../../habit detail/pages/habit_detail.dart';
 import '../../user_data/habit_provider.dart';
 import '../mixins/create_habit_mixin.dart';
@@ -29,26 +25,7 @@ class _CreateHabitPageState extends State<CreateHabitPage>
     with CreateHabitMixin {
   String response = "";
 
-  Future<void> generateResponse(
-    String? userContentText,
-    String? userContentImageUrl,
-    String systemContentText,
-    TextEditingController controller,
-  ) async {
-    controller.clear();
-    final body = ChatgptService().getApiBody(
-        systemContentText: systemContentText,
-        userContentText: userContentText,
-        imageUrl: userContentImageUrl);
-
-    var response = '';
-    ChatgptService().getChatResponse(body).listen((word) {
-      setState(() {
-        response += word;
-        controller.text = response;
-      });
-    });
-  }
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -72,70 +49,13 @@ class _CreateHabitPageState extends State<CreateHabitPage>
                   urlController: imageUrlController,
                 ),
                 const Gap(AppPaddings.smallPaddingValue),
-
-                /// Habit Name
-                NormalTextFormField(
-                  label: habitNameTextFieldLabel,
-                  controller: habitNameController,
-                  textCapitalization: TextCapitalization.words,
-                  maxLength: 25,
-                  buildCounter: (context,
-                      {required currentLength,
-                      required isFocused,
-                      required maxLength}) {
-                    return Text(
-                      "$currentLength / $maxLength",
-                      style: context.textTheme.labelLarge?.copyWith(
-                        color: Colors.white,
-                      ),
-                    );
-                  },
-                ),
-                const Gap(AppPaddings.smallPaddingValue),
-
-                /// Purpose text Field
-                TextAreaFormField(
-                  label: habitPurposeTextFieldLabel,
-                  suffixIcon: SizedBox(
-                    width: 32,
-                    height: 32,
-                    child: GestureDetector(
-                      onTap: () {
-                        if ((habitNameController.text.isNotEmpty) ||
-                            (imageUrlController.text.isNotEmpty)) {
-                          generateResponse(
-                            habitNameController.text,
-                            imageUrlController.text,
-                            purposeSystemContentText,
-                            purposeController,
-                          );
-                        } else {
-                          // TODO: Buraya ekrana yansıtılan Uyerı mesajı eklenebilir.
-                          print(
-                              'En az bir metin veya resim içeriği belirtmelisiniz.');
-                        }
-                      },
-                      child: Lottie.asset(AppAssets.aiButtonAnimation),
-                    ),
-                  ),
-                  constraints: BoxConstraints(
-                    maxHeight: context.dynamicHeight(0.18),
-                  ),
-                  controller: purposeController,
-                  textInputAction: TextInputAction.done,
-                  maxLength: 500,
-                  textCapitalization: TextCapitalization.sentences,
-                  buildCounter: (context,
-                      {required currentLength,
-                      required isFocused,
-                      required maxLength}) {
-                    return Text(
-                      "$currentLength / $maxLength",
-                      style: context.textTheme.labelLarge?.copyWith(
-                        color: Colors.white,
-                      ),
-                    );
-                  },
+                CreateHabitForm(
+                  habitNameTextFieldLabel: habitNameTextFieldLabel,
+                  habitNameController: habitNameController,
+                  habitPurposeTextFieldLabel: habitPurposeTextFieldLabel,
+                  purposeController: purposeController,
+                  formKey: formKey,
+                  imageUrlController: imageUrlController,
                 ),
               ],
             ),
@@ -164,34 +84,37 @@ class _CreateHabitPageState extends State<CreateHabitPage>
   }
 
   void onAddTapped() {
-    if (habitNameController.text.isNotEmpty) {
-      HabitModel habitModel = HabitModel(
-        title: habitNameController.text,
-        createDate: DateTime.now(),
-        streakCount: 0,
-        purpose: purposeController.text,
-        imageUrl: imageUrlController.text,
-        completionDates: [],
-        longestStreak: 0,
-      );
-      Provider.of<HabitProvider>(context, listen: false)
-          .addHabit(
-        habitModel,
-      )
-          .whenComplete(
-        () {
-          int length =
-              Provider.of<HabitProvider>(context, listen: false).habits.length;
+    if (mounted) {
+      if (formKey.currentState!.validate()) {
+        HabitModel habitModel = HabitModel(
+          title: habitNameController.text,
+          createDate: DateTime.now(),
+          streakCount: 0,
+          purpose: purposeController.text,
+          imageUrl: imageUrlController.text,
+          completionDates: [],
+          longestStreak: 0,
+        );
+        Provider.of<HabitProvider>(context, listen: false)
+            .addHabit(
+          habitModel,
+        )
+            .whenComplete(
+          () {
+            int length = Provider.of<HabitProvider>(context, listen: false)
+                .habits
+                .length;
 
-          if (length > 0) {
-            length--;
-          }
-          context.navigator.pushReplacementNamed(
-            HabitDetail.routeName,
-            arguments: length,
-          );
-        },
-      );
+            if (length > 0) {
+              length--;
+            }
+            context.navigator.pushReplacementNamed(
+              HabitDetail.routeName,
+              arguments: length,
+            );
+          },
+        );
+      }
     }
   }
 }
